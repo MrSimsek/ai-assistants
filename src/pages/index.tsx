1741291345d495216f45b112afe9d52cea47f24b
Deniz Simsek <denizsimsek93@gmail.com>
2023-03-16 14:06:49 +0300

    Refactor heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,13 +27,12 @@ export default function Home() {
       <main className="h-screen flex flex-col items-center py-12 px-4 bg-zinc-800">
         <div className="max-w-screen-xl flex flex-col items-center space-y-12 p-4">
           <div className="flex flex-col space-y-4 items-center">
-            <h1 className="text-3xl font-bold text-gray-200">
-              Discover the Latest AI Tools and Technologies for Your Business
+            <h1 className="text-6xl font-extrabold bg-gradient-to-r from-pink-500 to-yellow-500 inline-block text-transparent bg-clip-text">
+              AI Assistants
             </h1>
-            <h3 className="text-xl font-medium text-gray-300">
-              Explore a Wide Range of AI Tools and Resources to Help Your
-              Business Grow and Thrive
-            </h3>
+            <h2 className="text-2xl font-semibold text-gray-200">
+              Discover the Latest AI Tools and Technologies for Your Business
+            </h2>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {tools.map((tool) => (
